5d716ea4aa50cd28206e03408a813cf6c09a731d
kimo2508 <kimo2508@gmail.com>
2026-04-18 09:19:26 -0700

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+// Selah Service Worker - enables PWA install on Android
+const CACHE_NAME = 'selah-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
